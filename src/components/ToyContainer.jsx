3e2111d4a8f1e9e57f5b16f16315678d7a1177c1
Danielle <danielle.shokrian@aol.com>
2025-08-12 14:04:18 -0400

    Create toy form completed

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -21,13 +21,42 @@ function ToyContainer() {
       .then((createdToy) => setToys((toys) => [...toys, createdToy]));
   }
 
+    function handleDeleteToy(id) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.ok) {
+          setToys((prevToys) => prevToys.filter((toy) => toy.id !== id));
+        } else {
+          alert("Failed to delete toy");
+        }
+      });
+  }
+
+  function handleLikeToy(id, currentLikes) {
+    const updatedLikes = currentLikes + 1;
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: updatedLikes }),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => {
+        setToys((prevToys) =>
+          prevToys.map((toy) =>
+            toy.id === id ? updatedToy : toy
+          )
+        );
+      });
+  }
 
   return (
     <div>
       <ToyForm onAddToy={handleAddToy} />
       <div className="toy-container">
         {toys.map((toy) => (
-          <ToyCard key={toy.id} toy={toy} />
+          <ToyCard key={toy.id} toy={toy} onDeleteToy={handleDeleteToy} onLikeToy={handleLikeToy} />
         ))}
       </div>
     </div>
